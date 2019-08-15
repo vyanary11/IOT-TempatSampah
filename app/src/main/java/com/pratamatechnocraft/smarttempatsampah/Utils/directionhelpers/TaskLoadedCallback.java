10d5035f586b37da87d7be30ec4b7e39887d0ba1
Vyan-LaptopAsusBapak <vyanaryprabowo9763@gmail.com>
2019-08-15 18:33:02 +0700

    Helper Direction

---
+++ app/src/main/java/com/pratamatechnocraft/smarttempatsampah/Utils/directionhelpers/TaskLoadedCallback.java
@@ -0,0 +1,9 @@
+package com.pratamatechnocraft.smarttempatsampah.Utils.directionhelpers;
+
+/**
+ * Created by Vishal on 10/20/2018.
+ */
+
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
